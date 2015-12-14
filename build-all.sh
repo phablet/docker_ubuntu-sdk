4dd6d163d5bd342fbbfe7c87ec3d1bfd9da38dc4
You-Sheng Yang <vicamo@gmail.com>
2015-12-14 14:39:42 +0800

    push to phablet/ubuntu-sdk-frameworks

--- build-all.sh
+++ build-all.sh
@@ -6,11 +6,8 @@ cd $(dirname ${BASH_SOURCE})
 
 #DRYRUN=echo
 
-DOCKER_USER=$(docker info | grep Username: | awk '{print $2}')
-[ -n "${DOCKER_USER}" ] && DOCKER_USER=${DOCKER_USER}/
-
 for suite in `find . -mindepth 1 -maxdepth 1 -type d ! -name .git -printf %f\\\\n`; do
   echo "#### ${suite} ####"
 
-  ${DRYRUN} docker build --no-cache=true -t ${DOCKER_USER}ubuntu-sdk:${suite} ${suite}
+  ${DRYRUN} docker build --no-cache=true -t phablet/ubuntu-sdk:${suite} ${suite}
 done
