6545b5c478382749127089c9512329cc459c6183
You-Sheng Yang <vicamo@gmail.com>
2015-12-14 14:37:32 +0800

    remove sdk frameworks images

--- build-all.sh
+++ build-all.sh
@@ -9,7 +9,7 @@ cd $(dirname ${BASH_SOURCE})
 DOCKER_USER=$(docker info | grep Username: | awk '{print $2}')
 [ -n "${DOCKER_USER}" ] && DOCKER_USER=${DOCKER_USER}/
 
-for suite in `ls -d -1 *`; do
+for suite in `find . -mindepth 1 -maxdepth 1 -type d ! -name .git -printf %f\\\\n`; do
   echo "#### ${suite} ####"
 
   ${DRYRUN} docker build --no-cache=true -t ${DOCKER_USER}ubuntu-sdk:${suite} ${suite}
